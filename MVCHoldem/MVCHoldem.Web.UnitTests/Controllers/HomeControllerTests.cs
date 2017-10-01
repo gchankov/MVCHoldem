dc8f3e7965bec03832f70b195d60482f09b0aa77
Gancho Chankov <gchankov@gmail.com>
2017-10-02 01:57:12 +0300

    Adding Initial Project with NUnit3 tests, console runner, OpenCover and Coveralls badge.

---
+++ MVCHoldem/MVCHoldem.Web.UnitTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Web.Mvc;
+using MVCHoldem.Web.Controllers;
+using NUnit.Framework;
+
+namespace MVCHoldem.UnitTests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [Test]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MVCHoldem
 8th season of Telerik Academy - amateur ASP. Net MVC project
+
+[![Coverage Status](https://coveralls.io/repos/github/gchankov/MVCHoldem/badge.svg)](https://coveralls.io/github/gchankov/MVCHoldem)
