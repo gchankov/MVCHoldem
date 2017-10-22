40bf7c4151318ce8e63361ac46693b0f70ccd1ac
Gancho Chankov <gchankov@gmail.com>
2017-10-22 22:54:20 +0300

    Adding ChatController unit test.

---
+++ MVCHoldem/MVCHoldem.UnitTests/Web/Controllers/ChatControllerTests/Index_Should.cs
@@ -0,0 +1,22 @@
+﻿namespace MVCHoldem.UnitTests.Web.Controllers.ChatControllerTests
+{
+    using MVCHoldem.Web.Controllers;
+    using NUnit.Framework;
+    using TestStack.FluentMVCTesting;
+
+    [TestFixture]
+    public class Index_Should
+    {
+        [Test]
+        public void RenderDefaultViewWithHomeViewModelAndMostRecentPostPartialWithMostRecentPostViewModel_WhenCalled()
+        {
+            // Arrange
+            ChatController homeController = new ChatController();
+
+            // Act & Assert
+            homeController
+                .WithCallTo(c => c.Index())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
